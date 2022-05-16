d33205bd3668340c694bc67d72e51b23f85fad65
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-16 15:40:25 -0500

    exercise 1 complete

---
+++ 01 - JavaScript Drum Kit/Drum.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', function (e) {
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if(!audio) return;  //stops function if invalid key is entered
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    
+});
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //skip if not transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
